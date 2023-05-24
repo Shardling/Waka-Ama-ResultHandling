3fa0eaa9ac5645f16ab793f039a3c6b01ef639bf
Shardling <107162461+Shardling@users.noreply.github.com>
2023-05-25 08:59:25 +1200

    Functionality update
    can now sort by year and not just a keyword. this sorting however reads the folder name and not the file names.

--- Main.py
+++ Main.py
@@ -2,26 +2,38 @@
 import os
 
 #function for finding files with keywords and storing them in a list
-def Select_files_with_finals(folder_path: str, keyword: str) -> list:
-    #looks for the path
+def Select_files_with_finals(folder_path: str, keyword: str, keyword_2: str) -> list:
+    #looks for the path]
+    global folder_name
     absolute_folder_path = os.path.abspath(folder_path)
-    #the list
+    #the list(s)
     selected_files = []
+    other_files = []
+    #parses through each file name and checks if keyword is in it.
     for root, dirs, files in os.walk(absolute_folder_path): 
         for file_name in files: 
             if keyword in file_name: 
-                file_path = os.path.join(root, file_name) 
-                if os.path.isfile(file_path):
-                    selected_files.append(file_path) 
+                file_path = os.path.join(root, file_name)
+                #from those files, check if the folder they are in have keyword_2 
+                folder_name = os.path.dirname(file_path)
+                for root, dirs, files in os.walk(folder_name):
+                    #if it is in the folder name, append to selected_files
+                    if keyword_2 in folder_name:
+                        if os.path.isfile(file_path):
+                            selected_files.append(file_path)
+                    #else append to other_files list
+                    else:
+                        if os.path.isfile(file_path):
+                            other_files.append(file_path)
     #so I can use this outside of the function
-    return selected_files
-
+    return selected_files, other_files
 #These are where the folder path and keywords are located
 folder_path = "/Users/mtvpo/Downloads/3.7B resource files"
 keyword = "Final" 
+keyword_2 = "2017"
 
 #Tells the user if their folders have the necessary files in them, this is subject to change
-selected_files = Select_files_with_finals(folder_path, keyword) 
+selected_files = Select_files_with_finals(folder_path, keyword, keyword_2)
 if selected_files: 
     for file_path in selected_files: 
         with open(file_path, 'r') as file: 
@@ -29,3 +41,6 @@ if selected_files:
             print(f"File: '{file_path}':") 
 else: 
     print(f"No files with the keyword '{keyword}' found in the folder.") 
+
+pain = len(selected_files)
+print(pain)
